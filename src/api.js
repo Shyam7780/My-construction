c6bde7e0271b2fe38023452f6e5e0bad9ddefc11
Shyam7780 <yadav25bcs095@gmail.com>
2026-06-09 00:43:49 +0530

    api.js change

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+// src/api.js में यह होना चाहिए
+const API = axios.create({ 
+  baseURL: 'https://chhotan-ram-api1.onrender.com/api' 
+});
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
 export const loginAdmin = (data) => API.post('/auth/login', data);
 export const getRates = () => API.get('/data/rates');
